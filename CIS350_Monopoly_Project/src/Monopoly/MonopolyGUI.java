a64dea8738aa0e004603959a7ac761cd14252648
Ben Burger <benwburger45@gmail.com>
2019-07-10 12:54:06 -0400

    fixed spacing on GUI

--- CIS350_Monopoly_Project/src/Monopoly/MonopolyGUI.java
+++ CIS350_Monopoly_Project/src/Monopoly/MonopolyGUI.java
@@ -1,12 +1,15 @@
 package Monopoly;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 /**
  * GUI for the Monopoly game board.
@@ -17,41 +20,43 @@ import javax.swing.JMenuItem;
 @SuppressWarnings("serial")
 public class MonopolyGUI extends JFrame implements ActionListener {
 
-//	private static final long serialVersionUID = 1L; 
-	
+	//	private static final long serialVersionUID = 1L; 
+
 	private JMenuBar menus;
 	private JMenu mainMenu;
 	private JMenuItem exitItem;
-	
+
 	/** 
 	 * Default constructor for a Monopoly GUI.
 	 * @param String frameLabel - label for the window
 	 */
 	public MonopolyGUI(String frameLabel) {
-		
+
 		super(frameLabel);
-		
+
 		menus = new JMenuBar();
 		mainMenu = new JMenu("Menu");
 		exitItem = new JMenuItem("exit");
-		
+
 		mainMenu.add(exitItem);
 		menus.add(mainMenu);
-		
+
 		exitItem.addActionListener(this);
-		
+
 		this.setJMenuBar(menus);
-		
+
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		MonopolyPanel panel = new MonopolyPanel();
+
+		panel.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.blue));
 
 		this.getContentPane().add(panel);
 
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		this.setVisible(true);
 	}
-	
+
 	/**
 	 * Tells the GUI what to do when an action happens.
 	 * @param e - ActionEvent used to indicate action happened
@@ -69,5 +74,33 @@ public class MonopolyGUI extends JFrame implements ActionListener {
 	public static void main (String args[]) {
 		new MonopolyGUI("Monopoly Game");
 
+		int numOfPlayers = startGame();
+	}
+
+	public static int startGame() {
+
+
+		// asks user for the desired board size
+		boolean goodNum = false;
+		int numOfPlayers = 0;
+
+		// stays in loop until valid size is entered or
+		// user hits cancel
+		while (!goodNum) {
+			try {
+				String num = JOptionPane.showInputDialog(null, "Enter in the number of players: \n Between 2 and 4.");
+
+				numOfPlayers = Integer.parseInt(num);
+			}
+			catch(Exception e) {
+				System.exit(0);   // exit the program
+			}
+			if (numOfPlayers >= 2 && numOfPlayers <= 4)
+				goodNum = true;
+
+			else
+				JOptionPane.showMessageDialog(null, "Enter valid number of players.");
+		}
+		return numOfPlayers;
 	}
 }
--- CIS350_Monopoly_Project/src/Monopoly/MonopolyPanel.java
+++ CIS350_Monopoly_Project/src/Monopoly/MonopolyPanel.java
@@ -1,15 +1,18 @@
 package Monopoly;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 
 /**
@@ -23,7 +26,8 @@ public class MonopolyPanel extends JPanel {
 
 	private ButtonListener listener;
 	private BoardPanel board;
-	private JLabel gameInfo;
+	private JTextArea gameInfo;
+	private JScrollPane scrollPane;
 	private JButton rollButton;
 	private JButton endTurnButton;
 
@@ -38,51 +42,73 @@ public class MonopolyPanel extends JPanel {
 		GridBagConstraints c = new GridBagConstraints();
 
 		board = new BoardPanel();
-		//		c.gridx = 0;
-		//		c.gridy = 0;
-		c.fill = GridBagConstraints.VERTICAL;
-		//		c.ipadx = 0;
-		//		c.ipady = 0;
-		//		c.anchor = 0;
-		//		c.weightx = 0;
-		//		c.weighty = 0;
+		board.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.red));
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.gridheight = 3;
+//		c.fill = GridBagConstraints.VERTICAL;
+//		c.ipadx = 0;
+//		c.ipady = 0;
+//		c.insets = 0;
+//		c.anchor = GridBagConstraints.WEST;
+//		c.weightx = 1;
+//		c.weighty = 1;
 		this.add(board, c);
 
-		gameInfo = new JLabel("test");
+		gameInfo = new JTextArea("Welcome!");
+		for(int i=0; i<100;i++)
+			gameInfo.append("\nHello!");
+		gameInfo.setEditable(false);
+		scrollPane = new JScrollPane(gameInfo);
+		scrollPane.setPreferredSize(new Dimension(200, 400));
+		scrollPane.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
 		c.gridx = 1;
 		c.gridy = 0;
-		//		c.fill = 0;
-		//		c.ipadx = 0;
-		//		c.ipady = 0;
-		c.anchor = GridBagConstraints.NORTH;
-		//		c.weightx = 0;
-		//		c.weighty = 0;
-		this.add(gameInfo, c);
+		c.gridwidth = 1;
+		c.gridheight = 1;
+//		c.fill = NONE;
+		c.ipadx = 10;
+//		c.ipady = 0;
+		c.insets = new Insets(0, 20, 0, 0);			// (top, left, bottom, right)
+//		c.anchor = GridBagConstraints.NORTH;
+//		c.weightx = 0.5;
+//		c.weighty = 0.5;
+		this.add(scrollPane, c);
 
 		rollButton = new JButton("roll");
 		rollButton.addActionListener(listener);
+		rollButton.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
 		c.gridx = 1;
 		c.gridy = 1;
-		//		c.fill = 0;
-		//		c.ipadx = 0;
-		//		c.ipady = 0;
-		c.insets = new Insets(10, 50, 10, 50);
-//		c.anchor = GridBagConstraints.NORTH;
-		//		c.weightx = 0;
-		//		c.weighty = 0;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+//		c.fill = NONE;
+//		c.ipadx = 0;
+//		c.ipady = 0;
+		c.insets = new Insets(450, 20, 0, 0);		// (top, left, bottom, right)
+		c.anchor = GridBagConstraints.SOUTH;
+//		c.weightx = 0.5;
+//		c.weighty = 0.5;
 		this.add(rollButton, c);
 
 		endTurnButton = new JButton("end turn");
 		endTurnButton.addActionListener(listener);
+		endTurnButton.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
 		c.gridx = 1;
 		c.gridy = 2;
-		//		c.fill = 0;
-		//		c.ipadx = 0;
-		//		c.ipady = 0;
-		c.anchor = GridBagConstraints.NORTH;
-		//		c.weightx = 0;
-		//		c.weighty = 0;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+//		c.fill = NONE;
+//		c.ipadx = 0;
+//		c.ipady = 0;
+		c.insets = new Insets(0, 20, 0, 0);		// (top, left, bottom, right)
+		c.anchor = GridBagConstraints.SOUTH;
+//		c.weightx = 0.5;
+//		c.weighty = 0.5;
 		this.add(endTurnButton, c);
+		
+		
 	}
 
 
@@ -98,7 +124,7 @@ public class MonopolyPanel extends JPanel {
 		public void actionPerformed(ActionEvent e) {
 
 			if(e.getSource() == rollButton) {
-				
+
 			}
 
 			if(e.getSource() == endTurnButton) {
